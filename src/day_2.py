8db523f2a75c892cf4f06cb4fdcfb5ba0426677e
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2023-09-23 01:18:17 +0200

    comparison points

--- src/day_2.py
+++ src/day_2.py
@@ -1,30 +1,28 @@
-""" Determine the comparison points between two students."""
-
-def comparison_points(a: list, b: list) -> list:
+def comparison_points(student_a_scores: list, student_b_scores: list) -> list:
     """
-    Compares two lists and assign points.
+    Determine the comparison points between two students.
 
     :param a: First list
     :param b: Second list
     :return: List containing assigned points.
     """
     try:
-        assert isinstance(a, list) and isinstance(b, list)
-        assert len(a) == len(b)
+        assert isinstance(student_a_scores, list) and isinstance(student_b_scores, list)
+        assert len(student_a_scores) == len(student_b_scores)
         
-        a_points = 0
-        b_points = 0
-        for i in range(len(a)):
-            if a[i] > b[i]:
-                a_points += 1
-            elif a[i] < b[i]:
-                b_points += 1
+        student_a_points = 0
+        student_b_points = 0
+        for i in range(len(student_a_scores)):
+            if student_a_scores[i] > student_b_scores[i]:
+                student_a_points += 1
+            elif student_a_scores[i] < student_b_scores[i]:
+                student_b_points += 1
                 
-        return [a_points, b_points]
+        return [student_a_points, student_b_points]
                 
     except (AssertionError, TypeError):
         print("Error!!!\n1. Argument(s) not a list.\n2. Contains an unsupported data type.\n3. a and b are not equal in length")
     
 
-print(comparison_points([1, 3], [3, 2, 1]) )
+print(comparison_points([1, 2.5, 3], [3, 2, 1]) )
     
