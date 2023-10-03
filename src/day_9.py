27934917774ec22433dca301535a1f739a0aab34
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2023-10-03 22:20:34 +0200

    reversed order

---
+++ src/day_9.py
@@ -0,0 +1,12 @@
+def reverse_order(arg: str) -> str:
+    """
+    A function that returns a string in the reversed order.
+
+    :param arg: Input string
+    :return: Reversed string
+    """
+    return arg[::-1]
+
+print(reverse_order("pharmacy"))
+print(reverse_order("brown fox"))
+
